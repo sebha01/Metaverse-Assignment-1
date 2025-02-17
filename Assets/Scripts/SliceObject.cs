c053dc9024bbbaa573fed8cd03057044a86e8281
sebha01 <30077548@students.southwales.ac.uk>
2025-02-17 15:53:15 +0000

    Current work

--- Assets/Scripts/SliceObject.cs
+++ Assets/Scripts/SliceObject.cs
@@ -9,6 +9,7 @@ public class SliceObject : MonoBehaviour
     public Transform planeDebug;
     public GameObject target;
     public Material crossSectionMaterial;
+    public float cutForce = 2000;
 
     // Start is called before the first frame update
     void Start()
@@ -32,9 +33,20 @@ public class SliceObject : MonoBehaviour
         if (hull != null)
         {
             GameObject upperHull = hull.CreateUpperHull(target, crossSectionMaterial);
+            SetUpSlicedComponents(upperHull);
+            
             GameObject lowerHull = hull.CreateLowerHull(target, crossSectionMaterial);
+            SetUpSlicedComponents(lowerHull);
 
             Destroy(target);
         }
+    }
+
+    public void SetUpSlicedComponents(GameObject slicedObject)
+    {
+        Rigidbody rb = slicedObject.AddComponent<Rigidbody>();
+        MeshCollider collider = slicedObject.AddComponent<MeshCollider>();
+        collider.convex = true;
+        rb.AddExplosionForce(cutForce, slicedObject.transform.position, 1);
     }
 }
