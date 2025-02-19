04f1f5203a34e66cb970accc117bf2668fd65f90
sebha01 <30077548@students.southwales.ac.uk>
2025-02-19 14:56:34 +0000

    Current progress

--- Assets/Limb.cs
+++ Assets/Limb.cs
@@ -11,9 +11,9 @@ public class Limb : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Weapon"))
+        if (collision.gameObject.tag == "Weapon")
         {
-
+            Hit();
         }
     }
 }
